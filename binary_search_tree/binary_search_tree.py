408797521927af4227d23d096af1dfc32f5aecd1
Sandrava Philips <lacasandra1@gmail.com>
2020-02-25 21:46:12 +0100

    worked on parts of the binarysearchtree exercise

--- binary_search_tree/binary_search_tree.py
+++ binary_search_tree/binary_search_tree.py
@@ -1,7 +1,6 @@
 import sys
 sys.path.append('../queue_and_stack')
-from dll_queue import Queue
-from dll_stack import Stack
+from doubly_linked_list import DoublyLinkedList
 
 
 class BinarySearchTree:
@@ -9,24 +8,55 @@ class BinarySearchTree:
         self.value = value
         self.left = None
         self.right = None
+        self.storage = DoublyLinkedList()
+        self.storage.add_to_head(value)
 
     # Insert the given value into the tree
     def insert(self, value):
-        pass
+        if value < self.value:
+            if not self.left:
+                self.left = BinarySearchTree(value)
+            else:                
+                self.left.insert(value)
+        else:
+            if not self.right:
+                self.right = BinarySearchTree(value)
+            else: self.right.insert(value)
+        self.storage.add_to_head(value)
+        
 
     # Return True if the tree contains the value
     # False if it does not
     def contains(self, target):
-        pass
+        if target == self.value:
+            return True
+        elif target < self.value:
+            if not self.left:
+                return False
+            else:
+                return self.left.contains(target)
+        else:
+            if not self.right:
+                return False
+            else:
+                return self.right.contains(target)
 
     # Return the maximum value found in the tree
     def get_max(self):
-        pass
+        return self.storage.get_max()
+
+    def iter(self):
+        current = self.storage.head
+        while current:
+            item = current.value
+            current = current.next
+            yield item
 
     # Call the function `cb` on the value of each node
     # You may use a recursive or iterative approach
     def for_each(self, cb):
-        pass
+        for i in self.iter():
+            cb(i)
 
     # DAY 2 Project -----------------------
 
