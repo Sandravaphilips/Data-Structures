0466409b4ef48a1dae7a3382fcaa7cae2774e99f
Sandrava Philips <lacasandra1@gmail.com>
2020-02-27 21:35:01 +0100

    cleared out unnecessary code

--- binary_search_tree/binary_search_tree.py
+++ binary_search_tree/binary_search_tree.py
@@ -126,13 +126,3 @@ class BinarySearchTree:
         else: return
 
 
-bst = BinarySearchTree(1)
-bst.insert(8)
-bst.insert(5)
-bst.insert(7)
-bst.insert(6)
-bst.insert(3)
-bst.insert(4)
-bst.insert(2)
-
-bst.dft_print(bst)
